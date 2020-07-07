3bc9833f7414d0e86a1d0ec81828c5fe5541034b
yanyujianshen <ljh274163788@gmail.com>
2020-07-07 12:25:49 -0400

    ready for drawing

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -8,17 +8,27 @@
 
 import Foundation
 
-class Concentration{
+struct Concentration{
     
-    var cards = [Card]()
+    private(set) var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get{
+            return cards.indices.filter{cards[$0].isFaceUp}.oneAndOnly
+        }
+        set{
+            for index in cards.indices{
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     var flipCount = 0
     var score = 0
     var seenCard = Set<Int>()
     
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)):You must have at least one pair of cards")
         for _ in 0..<numberOfPairsOfCards{
             let card = Card()
             cards += [card, card]
@@ -26,14 +36,15 @@ class Concentration{
         cards.shuffle()
     }
     
-    func chooseCard(at index : Int){
+    mutating func chooseCard(at index : Int){
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at:\(index)):chosen index not in the cards")
 //        1. All cards face down, the chosen one gets faced up
 //        2. One card is already facing up, the chosen one needs to check if it matches or not
 //        3. Two cards facing up, the new chosen one gets faced up, flip the previous two down
-        flipCount+=1
         if !cards[index].isMatched{
+            flipCount+=1
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index{
-                if cards[matchIndex].identifier == cards[index].identifier{
+                if cards[matchIndex] == cards[index]{
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                     score += 2
@@ -50,26 +61,27 @@ class Concentration{
                     seenCard.insert(index)
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             }else{
                 //either no cards or 2 cards are face up
                 //flip every card to face down
-                for flipDownIndex in cards.indices{
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true;
                 indexOfOneAndOnlyFaceUpCard = index
                 seenCard.insert(indexOfOneAndOnlyFaceUpCard!)
             }
         }
     }
     
-    func resetGame(){
+    mutating func resetGame(){
         flipCount = 0
         score = 0
         for flipDownIndex in cards.indices{
             cards[flipDownIndex].isFaceUp = false
             cards[flipDownIndex].isMatched = false
         }
+    }
+}
+
+extension Collection{
+    var oneAndOnly: Element?{
+        return count == 1 ? first : nil
     }
 }
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,37 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count+1) / 2)
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+
+    var numberOfPairsOfCards: Int{
+        return (cardButtons.count+1) / 2
+    }
     
-    var flipCount:Int = 0{
+    private(set) var flipCount:Int = 0{
         didSet{ //property observer
-            flipCountLabel.text = "Flip:\(flipCount)"
+            updateFlipCountLabel()
         }
     }
     
-    var score:Int = 0{
+    private func updateFlipCountLabel(){
+        let attributes: [NSAttributedString.Key:Any] = [
+            .strokeWidth : 5.0,
+            .strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        ]
+        let attributedString = NSAttributedString(string: "Flip:\(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
+    
+    private(set) var score:Int = 0{
         didSet{
             scoreLabel.text = "Score:\(score)"
         }
     }
     
-    var emojiChoice = ["🏳️‍🌈", "🇺🇸", "🇯🇵","🇨🇳", "🏴󠁧󠁢󠁥󠁮󠁧󠁿", "🇨🇦","🇲🇽", "🇻🇳"]
-    var emojiChoices = ["Flags":["🏳️‍🌈", "🇺🇸", "🇯🇵","🇨🇳", "🏴󠁧󠁢󠁥󠁮󠁧󠁿", "🇨🇦","🇲🇽", "🇻🇳"],
-                        "Animals":["🐶", "🐒", "🦇", "🐴", "🐬", "🐇", "🐉", "🦍"],
-                        "Faces":["😀","🤓","🤪", "🥺", "🥶", "🤭", "🤮", "👿"]]
+    var emojiChoice = "🏳️‍🌈🇺🇸🇯🇵🇨🇳🏴󠁧󠁢󠁥󠁮󠁧󠁿🇨🇦🇲🇽🇻🇳"
+//    var emojiChoices = ["Flags":["🏳️‍🌈", "🇺🇸", "🇯🇵","🇨🇳", "🏴󠁧󠁢󠁥󠁮󠁧󠁿", "🇨🇦","🇲🇽", "🇻🇳"],
+//                        "Animals":["🐶", "🐒", "🦇", "🐴", "🐬", "🐇", "🐉", "🦍"],
+//                        "Faces":["😀","🤓","🤪", "🥺", "🥶", "🤭", "🤮", "👿"]]
     
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel!{
+        didSet{
+            updateFlipCountLabel()
+        }
+    }
     
-    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet private weak var scoreLabel: UILabel!
     
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private var cardButtons: [UIButton]!
     
-    @IBAction func newGame(_ sender: UIButton) {
+    @IBAction private func newGame(_ sender: UIButton) {
         game.resetGame()
         updateViewFromModel()
     }
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         if let cardNumber = cardButtons.firstIndex(of: sender){
             game.chooseCard(at: cardNumber)
             updateViewFromModel()
@@ -49,7 +66,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func updateViewFromModel(){
+    private func updateViewFromModel(){
         //from model to view, check isFaceUp or not to set the card view
         for index in cardButtons.indices{
             let button = cardButtons[index]
@@ -66,16 +83,17 @@ class ViewController: UIViewController {
         score = game.score
     }
     
-    var emoji = Dictionary<Int, String>()
+    private var emoji = Dictionary<Card, String>()
     
-    func emoji(for card : Card) -> String{
-        if emoji[card.identifier] == nil {
+    private func emoji(for card : Card) -> String{
+        if emoji[card] == nil {
             if emojiChoice.count > 0{
-                let randomIndex = Int.random(in: 0..<emojiChoice.count)
-                emoji[card.identifier] = emojiChoice.remove(at: randomIndex)
+                let randomStringIndex = emojiChoice.index(emojiChoice.startIndex, offsetBy: Int.random(in: 0..<emojiChoice.count))
+                emoji[card] = String(emojiChoice.remove(at: randomStringIndex))
             }
         }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
+    
 }
 
